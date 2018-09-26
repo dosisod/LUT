9387a4cfa5ccd449f97885d73b4d752f5f7eef72
dosisod <39638017+dosisod@users.noreply.github.com>
2018-09-25 22:37:28 -0700

    added some charsets

--- lut.py
+++ lut.py
@@ -7,3 +7,8 @@ class lut:
 		self.combo=args[1].split(" ")
 
 		self.hasher=hashlib.new(self.algo)
+
+		self.charsets={
+			"upper":['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+			"lower":['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+			"num":['0','1','2','3','4','5','6,','7','8','9']}
